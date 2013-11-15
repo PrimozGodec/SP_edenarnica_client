f0dce31577f4d649d9f6f0cfb4ba02a8fe4cdee9
primoz <p.godec9@gmail.com>
2013-11-15 15:57:01 +0100

    init

--- canvasDraw.js
+++ canvasDraw.js
@@ -22,6 +22,14 @@ var arcSize = width > height ? height : width;
 var posX = width / 2;
 var posY = height / 2;
 
+// calculate position of text
+var textXprih = posX + 1/2 * Math.cos(prihAng/2) * arcSize / 2;
+var textYprih = posY + 1/2 * Math.sin(prihAng/2) * arcSize / 2;
+
+var textXodh = posX + 1/2 * Math.cos(prihAng + odhAng/2) * arcSize / 2;
+var textYodh = posY + 1/2 * Math.sin(prihAng + odhAng/2) * arcSize / 2;
+
+
 // draw prih
 ctx.beginPath();
 ctx.fillStyle = "#00ce14";
@@ -36,5 +44,11 @@ ctx.strokeStyle = "#ff2020";
 ctx.arc(posX, posY, arcSize / 2  , prihAng, odhAng + prihAng);
 ctx.lineTo(posX, posY);
 ctx.fill();
+
+ctx.fillStyle = "#FFFFFF";
+ctx.strokeStyle = "#000000";
+ctx.font="18px Arial";
+ctx.fillText("Prihodki", textXprih -30, textYprih);
+ctx.fillText("Odhodki", textXodh - 30, textYodh);
 
 ctx.stroke();
